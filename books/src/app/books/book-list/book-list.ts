a21de3b127dcea2b04a239977dbf64b217e96a0a
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-17 08:59:56 +0200

    Bücherliste angefangen

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,9 +1,24 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'book-list',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList { }
+export class BookList {
+
+  books = [
+    { isbn: '123-456-789', title: 'Angular 21', price: 19.99 },
+    { isbn: '987-654-321', title: 'Angular 22', price: 29.99 }
+  ];
+
+  userName = 'Peter';
+
+  deleteBook(isbn: string) {
+    console.log(`Lösche Buch mit ISBN ${isbn}`);
+  }
+
+
+}
