714e8a48fa886aab5b9260eda995339f9176c1ad
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-17 09:45:40 +0200

    control flow #2

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class BookList {
 
-  protected books = [
-    { isbn: '123-456-789', title: 'Angular 21', price: 19.99 },
-    { isbn: '987-654-321', title: 'Angular 22', price: 29.99 }
+  protected books: Array<any> | null = [
+    { isbn: '123-456-789', title: 'Angular 21', price: 19.99, coverUrl: 'https://angular.io/assets/images/logos/angular/angular.png' },
+    { isbn: '987-654-321', title: 'Angular 22', price: 29.99, coverUrl: 'https://angular.io/assets/images/logos/angular/angular.png' }
   ];
 
   userName: string = 'Peter';
