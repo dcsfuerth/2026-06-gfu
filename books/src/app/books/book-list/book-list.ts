53684130219f0e3356fb003426ecbadc71262ab5
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-17 10:39:08 +0200

    Cover ein/ausschalten

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,25 +1,31 @@
-import { JsonPipe } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'book-list',
-  imports: [JsonPipe],
+  imports: [NgClass],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
+  // encapsulation: ViewEncapsulation.None
 })
 export class BookList {
 
   protected books: Array<any> | null = [
-    { isbn: '123-456-789', title: 'Angular 21', price: 19.99, coverUrl: 'https://angular.io/assets/images/logos/angular/angular.png' },
-    { isbn: '987-654-321', title: 'Angular 22', price: 29.99, coverUrl: 'https://angular.io/assets/images/logos/angular/angular.png' }
+    { isbn: '123-456-789', title: 'Angular 21', price: 19.99, coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg' },
+    { isbn: '987-654-321', title: 'Angular 22', price: 29.99, coverUrl: 'https://m.media-amazon.com/images/I/71xR-hhRjmL._AC_UY218_.jpg' }
   ];
 
   userName: string = 'Peter';
+
+  coverVisible: boolean = true;
 
   deleteBook(isbn: string) {
     // const booksJson = JSON.stringify(this.books);
     console.log(`Lösche Buch mit ISBN ${isbn}`);
   }
 
+  toggleCover() {
+    this.coverVisible = !this.coverVisible;
+  }
 
 }
