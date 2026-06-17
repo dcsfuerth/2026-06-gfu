835a81c1ab0dcef283ee0f89ba4331253bd1d444
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-17 13:27:36 +0200

    deutsches Lokale

--- books/src/app/app.config.ts
+++ books/src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { registerLocaleData } from "@angular/common";
+import localeDe from "@angular/common/locales/de";
+import localeDeExtra from "@angular/common/locales/extra/de";
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+registerLocaleData(localeDe, "de-DE", localeDeExtra);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: "de-DE" },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: "EUR" },
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     importProvidersFrom(FormsModule)
--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,10 +1,11 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'book-list',
-  imports: [FormsModule],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
   // encapsulation: ViewEncapsulation.None
@@ -13,7 +14,7 @@ export class BookList {
 
   protected books: Array<any> | null = [
     { isbn: '123-456-789', title: 'Angular 21', price: 19.99, coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg' },
-    { isbn: '987-654-321', title: 'Angular 22', price: 29.99, coverUrl: 'https://m.media-amazon.com/images/I/71xR-hhRjmL._AC_UY218_.jpg' }
+    { isbn: '987-654-321', title: 'Angular 22', price: 29.9, coverUrl: 'https://m.media-amazon.com/images/I/71xR-hhRjmL._AC_UY218_.jpg' }
   ];
 
   userName: string = 'Peter';
