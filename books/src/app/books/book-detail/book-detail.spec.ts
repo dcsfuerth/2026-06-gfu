5c8f0ca571e47b8c91e9c3c5669984ae51385e9d
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-18 20:05:04 +0200

    Tests angepasst

--- books/src/app/books/book-detail/book-detail.spec.ts
+++ books/src/app/books/book-detail/book-detail.spec.ts
@@ -1,19 +1,39 @@
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { BookDetail } from './book-detail';
 
 describe('BookDetail', () => {
   let component: BookDetail;
   let fixture: ComponentFixture<BookDetail>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BookDetail],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BookDetail);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // ohne isbn-Route-Param lädt ngOnInit kein Buch (kein HTTP-Aufruf)
+    fixture.detectChanges();
     await fixture.whenStable();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
--- books/src/app/books/book-list/book-list.spec.ts
+++ books/src/app/books/book-list/book-list.spec.ts
@@ -1,19 +1,42 @@
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { BookList } from './book-list';
 
 describe('BookList', () => {
   let component: BookList;
   let fixture: ComponentFixture<BookList>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BookList],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BookList);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // löst ngOnInit aus, das BookData.getBooks() aufruft
+    fixture.detectChanges();
+
+    // den HTTP-Aufruf aus ngOnInit abfangen und mit leerer Liste beantworten
+    httpMock.expectOne((req) => req.url.endsWith('/books')).flush([]);
     await fixture.whenStable();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
