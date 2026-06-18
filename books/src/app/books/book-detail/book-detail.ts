f69de6f09eb1c715cecb92bb092e02f466214793
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-18 13:18:13 +0200

    book-detail verbessert

--- books/src/app/books/book-detail/book-detail.ts
+++ books/src/app/books/book-detail/book-detail.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BookData } from '../bookdata/book-data';
 import { Book } from '../book';
 import { CurrencyPipe } from '@angular/common';
@@ -15,11 +15,34 @@ export class BookDetail implements OnInit {
   protected book: Book | undefined;
   protected readonly maxStars = Array.from({ length: 5 });
 
-  constructor(private router: Router, private bookDataService: BookData, private cd: ChangeDetectorRef) { }
+  constructor(
+    private router: Router,
+    private bookDataService: BookData,
+    private cd: ChangeDetectorRef,
+    private route: ActivatedRoute) { }
 
   async ngOnInit(): Promise<void> {
-    this.book = await this.bookDataService.getBook('123-456-789');
+
+    const isbn = this.route.snapshot.paramMap.get('isbn');
+
+    if (isbn) {
+      this.book = await this.bookDataService.getBook(isbn);
+    } else {
+      this.book = undefined;
+    }
+
     this.cd.detectChanges();
+
+    // this.route.paramMap.subscribe(async params => {
+    //   const isbn = params.get('isbn');
+    //   if (isbn) {
+    //     this.book = await this.bookDataService.getBook(isbn);
+    //   } else {
+    //     this.book = undefined;
+    //   }
+    //   this.cd.markForCheck();
+    // }
+
   }
 
   goBack() {
