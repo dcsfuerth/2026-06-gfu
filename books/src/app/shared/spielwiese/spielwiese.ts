534abfeb61bacaf906689d1b0c4811646a66332b
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-06-19 09:29:58 +0200

    ReplaySubject

--- books/src/app/shared/spielwiese/spielwiese.ts
+++ books/src/app/shared/spielwiese/spielwiese.ts
@@ -1,25 +1,46 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { map, Subject, timer } from 'rxjs';
-
+import { FormsModule } from '@angular/forms';
+import { ReplaySubject } from 'rxjs';
 @Component({
   selector: 'books-spielwiese',
-  imports: [],
+  imports: [AsyncPipe, FormsModule],
   templateUrl: './spielwiese.html',
   styleUrl: './spielwiese.css',
 })
 export class Spielwiese {
 
-  private mySubject = new Subject<string>();
+  protected mySubject$ = new ReplaySubject(5);
+  nachrichtentext: string = '';
 
   constructor() {
     // const obs3$ = timer(0, 500); // läuft endlos
     // const obs4$ = obs3$.pipe(map(value => value * 2));
     // obs4$.subscribe(console.log);
 
-    this.mySubject.next('Hallo');
-    this.mySubject.subscribe(value => console.log('Subscriber 1:', value));
-    this.mySubject.next('Welt');
-    this.mySubject.subscribe(value => console.log('Subscriber 2:', value));
+    // this.mySubject.next('Hallo');
+    // this.mySubject.subscribe(value => console.log('Subscriber 1:', value));
+    // this.mySubject.next('Welt');
+    // this.mySubject.subscribe(value => console.log('Subscriber 2:', value));
+
+    // // Startwert muß angegeben werden
+    // this.mySubject$.subscribe(e => console.log(1, e));
+    // // next() überschreibt den Startwert
+    // this.mySubject$.next('ich glaube schon');
+    // this.mySubject$.subscribe(e => console.log(2, e));
+    // this.mySubject$.next('na klar');
+
+    this.mySubject$.next('ich glaube schon');
+    this.mySubject$.next('na klar1');
+    this.mySubject$.next('na klar2');
+    this.mySubject$.next('na klar3');
+    this.mySubject$.subscribe(e => console.log(e));
+
+
+  }
+
+  nachricht(message: string) {
+    this.mySubject$.next(message);
   }
 
 
